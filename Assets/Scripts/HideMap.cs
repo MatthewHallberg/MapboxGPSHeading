676b23b408b7d927a54083cbab649aa9f9be6e3c
matthew hallberg <matthewhallberg@gmail.com>
2017-12-24 15:43:59 -0500

    Hid mapbox map, modified update distance and desired accuracy on device location provider.

---
+++ Assets/Scripts/HideMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		HideMapCoroutine = StartCoroutine (HideMapRoutine ());
+	}
+
+	Coroutine HideMapCoroutine;
+	IEnumerator HideMapRoutine(){
+		while (true) {
+			yield return new WaitForEndOfFrame ();
+			if (transform.childCount > 5) {
+				DisableChildren ();
+			}
+		}
+	}
+
+	void DisableChildren(){
+		StopCoroutine(HideMapCoroutine);
+		foreach (Transform child in this.transform) {
+			if (child.GetComponent<MeshRenderer>() != null || child.GetComponent<LineRenderer>() != null) {
+				child.gameObject.SetActive (false);
+			}
+		}
+	}
+}
